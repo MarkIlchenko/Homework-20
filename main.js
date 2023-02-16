7d4c94daff82e120436aa4fd9e710d1bb5ffbf33
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-02-16 21:30:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+function openCity(evt, cityName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+document.getElementById("defaultOpen").click();
+
+
+// Alert message
+
+let buttons = document.getElementsByTagName('a');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+        alert('Thank you for your purchase');
+    };
+
+}
